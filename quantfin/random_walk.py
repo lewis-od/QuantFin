e9fd1b3422bdf3cfb98fc9be6cf5d5247ac77ae0
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-05-07 00:31:19 +0100

    Modify mean_reversion and add docstring

--- quantfin/random_walk.py
+++ quantfin/random_walk.py
@@ -3,6 +3,7 @@ import numpy as np
 def geometric_brownian(N, S0, mu, sigma):
     """
     Generates a geometric Brownian random walk
+
     :param N: How many steps to take
     :param S0: Starting value of the walk
     :param mu: Drift parameter
@@ -15,10 +16,19 @@ def geometric_brownian(N, S0, mu, sigma):
 
     return S
 
-def mean_reversion(N, x0, X, alpha, mu=0.0, sigma=1.0):
+def mean_reversion(N, x0, X, alpha, sigma=1.0):
+    """
+    Generates a mean reverting random walk
+
+    :param N: How many steps to take
+    :param x0: Starting value
+    :param X: Mean value to revert to
+    :param alpha: Strength of mean reversion
+    :param sigma: Standard deviation of noise
+    """
     x = np.zeros((N,))
     x[0] = x0
     for n in range(1, N):
-        x[n] = x[n-1] + alpha * (X - x[n-1]) + np.random.normal(mu, sigma)
+        x[n] = x[n-1] + alpha * (X - x[n-1]) + np.random.normal(0.0, sigma)
 
     return x
