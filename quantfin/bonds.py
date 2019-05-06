2e97572b517aa05358ba1c24a30ec48307962cc7
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-05-06 23:04:03 +0100

    Plot yield curve for TR32

--- quantfin/bonds.py
+++ quantfin/bonds.py
@@ -1,22 +1,25 @@
 import numpy as np
 from scipy.optimize import root
 
-def yield_to_maturity(M, P, C, T, t, ti):
+def yield_to_maturity(M, P, C, T, t, m):
     """
     Calculates the yield to maturity of a bond
 
     :param M: Market value
     :param P: Principal
-    :param C: Coupon payment
+    :param C: Coupon payments
     :param T: Redemtion time
     :param t: Current time
-    :param ti: Times at which coupon payments are made
+    :param m: Fraction of the year coupon payments are made
+              (e.g. 0.5 for bi-annually)
     """
-    ti = ti[ti >= t] # Only consider future coupon payments
+    ti = np.arange(t, T, m)
 
+    # M = P*exp[-y(T - t))] + sum_{i}[C*exp(-y(T - t_i))]
     def f(y):
-        return P*np.exp(-y*(T - t)) + C * np.exp(-y *(ti - t)).sum() - M
-    y0 = 3*C / P
+        return P*np.exp(-y*(T - t)) + C*np.exp(-y *(ti - t)).sum() - M
+
+    y0 = 3*C / P # Initial guess - midpoint of 2C/P and 4C/P
     res = root(f, y0)
     if res.success:
         return res.x[0]
--- yield_tr32.py
+++ yield_tr32.py
@@ -4,11 +4,27 @@ import numpy as np
 # Parameters for TR32 bonds
 M = 133.9 # Market price (as of May 2019)
 P = 100.0 # Principal
-C = 4.35 # Coupon
-T = (32 - 19) # Years until maturity
+C = 4.35 / 2 # Coupon
+# T = (32 - 19) # Years until maturity
+T = 13
 t = 0  # Current time
-ti = np.arange(t, T, 0.5) # Coupon payments every 6 months
+m = 0.5 # Coupon payments every 6 months
 
-y = yield_to_maturity(M, P, C, T, t, ti)
+y = yield_to_maturity(M, P, C, T, t, m)
 
 print("y = {0:.2%}".format(y))
+
+# Plot yield curve
+Ts = np.arange(5, 25)
+ys = np.array([yield_to_maturity(M, P, C, Ti, t, m) for Ti in Ts])
+
+# Only plot positive yields
+Ts = Ts[ys > 0]
+ys = ys[ys > 0]
+
+import matplotlib.pyplot as plt
+plt.plot(Ts, ys * 100)
+plt.xlabel("Time to maturity (years)")
+plt.ylabel("Yield (%)")
+plt.title("Yield curve for TR32")
+plt.show()
