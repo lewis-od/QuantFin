05c9a7b50f506307078348fb9105a2eab2bef017
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-05-15 18:11:16 +0100

    Add module-level docstring

--- quantfin/__init__.py
+++ quantfin/__init__.py
@@ -1,3 +1,16 @@
+"""
+A python module for performing various quantitative finance calculations. Very
+much work in progress.
+
+- `quantfin.bonds` has a function to calculate the yield to maturity of a bond
+- `quantfin.random_walk` generates geometric Brownian or mean reverting random
+walks (for use in Monte Carlo simulations)
+- `quantfin.swaps` contains a function to calculate the fixed rate to use in a
+interest-rate swap
+- `quantfin.volatility` contains functions to calculate volatility from returns
+data using various models
+"""
+
 from . import bonds
 from . import swaps
 from . import random_walk
